8a2bf7a1a7a83a8041f60bcee0d75b34e44bef35
Dominic Zirbel <dominiczirbel@gmail.com>
2021-03-13 21:06:13 -0800

    Add simple grid for saved albums

--- src/main/kotlin/com/dominiczirbel/ui/Albums.kt
+++ src/main/kotlin/com/dominiczirbel/ui/Albums.kt
@@ -1,21 +1,35 @@
 package com.dominiczirbel.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.dominiczirbel.cache.SpotifyCache
+import com.dominiczirbel.ui.common.Grid
+import com.dominiczirbel.ui.common.LoadedImage
 import com.dominiczirbel.ui.theme.Dimens
 import com.dominiczirbel.ui.util.RemoteState
+
+private val IMAGE_SIZE = 200.dp
+private val CELL_ROUNDING = 8.dp
 
 @Composable
 fun BoxScope.Albums() {
     val state = RemoteState.of {
-        SpotifyCache.Albums.getSavedAlbums().map { SpotifyCache.Albums.getAlbum(it) }
+        SpotifyCache.Albums.getSavedAlbums()
+            .map { SpotifyCache.Albums.getAlbum(it) }
+            .sortedBy { it.name }
     }
 
     ScrollingPage(state) { albums ->
@@ -24,11 +38,31 @@ fun BoxScope.Albums() {
 
             Spacer(Modifier.height(Dimens.space3))
 
-            albums.forEach { album ->
-                Text(
-                    text = album.name,
-                    modifier = Modifier.padding(vertical = Dimens.space2)
-                )
+            Grid(
+                elements = albums,
+                horizontalSpacing = Dimens.space2,
+                verticalSpacing = Dimens.space3,
+                verticalCellAlignment = Alignment.Top
+            ) { album ->
+                Column(
+                    Modifier
+                        .clip(RoundedCornerShape(CELL_ROUNDING))
+                        .clickable { /* TODO open album page */ }
+                        .padding(Dimens.space3)
+                ) {
+                    LoadedImage(
+                        url = album.images.firstOrNull()?.url,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(Modifier.height(Dimens.space2))
+
+                    Text(
+                        text = album.name,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterHorizontally).widthIn(max = IMAGE_SIZE)
+                    )
+                }
             }
         }
     }
