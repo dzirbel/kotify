2a64277c38e71f58998efc3a0b2064e1557e1757
Dominic Zirbel <dominiczirbel@gmail.com>
2021-04-11 13:07:00 -0700

    Add play button to album grid cells

--- src/main/kotlin/com/dominiczirbel/ui/Albums.kt
+++ src/main/kotlin/com/dominiczirbel/ui/Albums.kt
@@ -1,6 +1,5 @@
 package com.dominiczirbel.ui
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
@@ -8,9 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -18,16 +14,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.style.TextAlign
 import com.dominiczirbel.cache.SpotifyCache
 import com.dominiczirbel.network.model.Album
 import com.dominiczirbel.ui.common.Grid
 import com.dominiczirbel.ui.common.InvalidateButton
-import com.dominiczirbel.ui.common.LoadedImage
 import com.dominiczirbel.ui.common.PageStack
 import com.dominiczirbel.ui.theme.Dimens
-import com.dominiczirbel.ui.util.mutate
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
@@ -100,27 +92,7 @@ fun BoxScope.Albums(pageStack: MutableState<PageStack>) {
                 horizontalSpacing = Dimens.space2,
                 verticalSpacing = Dimens.space3,
                 verticalCellAlignment = Alignment.Top
-            ) { album ->
-                Column(
-                    Modifier
-                        .clip(RoundedCornerShape(Dimens.cornerSize))
-                        .clickable { pageStack.mutate { to(AlbumPage(albumId = album.id!!)) } }
-                        .padding(Dimens.space3)
-                ) {
-                    LoadedImage(
-                        url = album.images.firstOrNull()?.url,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-
-                    Spacer(Modifier.height(Dimens.space2))
-
-                    Text(
-                        text = album.name,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.align(Alignment.CenterHorizontally).widthIn(max = Dimens.contentImage)
-                    )
-                }
-            }
+            ) { album -> AlbumCell(album, pageStack) }
         }
     }
 }
