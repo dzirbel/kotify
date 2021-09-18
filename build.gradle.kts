43f36562569361366384b61843c0cbb257f32801
Dominic Zirbel <dominiczirbel@gmail.com>
2021-09-18 14:24:16 -0700

    Upgrade slf4j 1.7.30 -> 1.7.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ private object Versions {
     const val junit = "5.8.0" // https://junit.org/junit5/
     const val kotlinReflect = "1.5.10" // should match kotlin version
     const val kotlinxSerialization = "1.0.1" // https://github.com/Kotlin/kotlinx.serialization
-    const val slf4j = "1.7.30" // http://www.slf4j.org/
+    const val slf4j = "1.7.32" // http://www.slf4j.org/
     const val ktor = "1.5.4" // https://ktor.io/changelog/
     const val mockk = "1.11.0" // https://mockk.io/
     const val okhttp = "4.9.1" // https://square.github.io/okhttp/
