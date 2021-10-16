97c6e08ce6c6890cbe058afde832f7686c541abe
Dominic Zirbel <dominiczirbel@gmail.com>
2021-10-16 16:40:21 -0700

    Add screenshot tests for SidePanel

---
+++ src/test/kotlin/com/dzirbel/kotify/ui/components/panel/SidePanelTest.kt
@@ -0,0 +1,59 @@
+package com.dzirbel.kotify.ui.components.panel
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dzirbel.kotify.ui.screenshotTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+
+internal class SidePanelTest {
+    @ParameterizedTest
+    @EnumSource(PanelDirection::class)
+    fun testFixed(direction: PanelDirection) {
+        screenshotTest(
+            filename = "fixed-${direction.name.lowercase()}"
+        ) {
+            SidePanel(
+                direction = direction,
+                panelSize = PanelSize(initialSize = FixedOrPercent.Fixed(200.dp)),
+                panelContent = {
+                    Box(Modifier.fillMaxSize()) {
+                        Text("Panel", modifier = Modifier.align(Alignment.Center))
+                    }
+                },
+                mainContent = {
+                    Box(Modifier.fillMaxSize()) {
+                        Text("Main content", modifier = Modifier.align(Alignment.Center))
+                    }
+                }
+            )
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(PanelDirection::class)
+    fun testPercent(direction: PanelDirection) {
+        screenshotTest(
+            filename = "percent-${direction.name.lowercase()}"
+        ) {
+            SidePanel(
+                direction = direction,
+                panelSize = PanelSize(initialSize = FixedOrPercent.Percent(0.25f)),
+                panelContent = {
+                    Box(Modifier.fillMaxSize()) {
+                        Text("Panel", modifier = Modifier.align(Alignment.Center))
+                    }
+                },
+                mainContent = {
+                    Box(Modifier.fillMaxSize()) {
+                        Text("Main content", modifier = Modifier.align(Alignment.Center))
+                    }
+                }
+            )
+        }
+    }
+}
