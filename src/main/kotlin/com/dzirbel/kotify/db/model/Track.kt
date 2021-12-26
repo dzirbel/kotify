0d91b8ca2e7dbdae3d1c7c42df186b2360a2aea1
Dominic Zirbel <dominiczirbel@gmail.com>
2021-12-26 12:49:00 -0800

    Finish database schema for tracks

--- src/main/kotlin/com/dzirbel/kotify/db/model/Track.kt
+++ src/main/kotlin/com/dzirbel/kotify/db/model/Track.kt
@@ -6,24 +6,66 @@ import com.dzirbel.kotify.db.SpotifyEntityClass
 import com.dzirbel.kotify.db.SpotifyEntityTable
 import com.dzirbel.kotify.network.Spotify
 import com.dzirbel.kotify.network.model.FullSpotifyTrack
+import com.dzirbel.kotify.network.model.SimplifiedSpotifyTrack
 import com.dzirbel.kotify.network.model.SpotifyTrack
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.SizedIterable
+import org.jetbrains.exposed.sql.Table
 import java.time.Instant
 
 object TrackTable : SpotifyEntityTable(name = "tracks") {
+    val discNumber: Column<UInt> = uinteger("disc_number")
     val durationMs: Column<ULong> = ulong("duration_ms")
+    val explicit: Column<Boolean> = bool("explicit")
+    val local: Column<Boolean> = bool("local")
+    val playable: Column<Boolean?> = bool("playable").nullable()
+    val trackNumber: Column<UInt> = uinteger("track_number")
+    val popularity: Column<UInt?> = uinteger("popularity").nullable()
+
+    val album: Column<EntityID<String>?> = reference("album", AlbumTable).nullable()
+
+    object TrackArtistTable : Table() {
+        val track = reference("track", TrackTable)
+        val artist = reference("artist", ArtistTable)
+        override val primaryKey = PrimaryKey(track, artist)
+    }
 }
 
 class Track(id: EntityID<String>) : SpotifyEntity(id = id, table = TrackTable) {
+    var discNumber: UInt by TrackTable.discNumber
     var durationMs: ULong by TrackTable.durationMs
+    var explicit: Boolean by TrackTable.explicit
+    var local: Boolean by TrackTable.local
+    var playable: Boolean? by TrackTable.playable
+    var trackNumber: UInt by TrackTable.trackNumber
+    var popularity: UInt? by TrackTable.popularity
+
+    var album: Album? by Album optionalReferencedOn TrackTable.album
+
+    var artists: SizedIterable<Artist> by Artist via TrackTable.TrackArtistTable
 
     companion object : SpotifyEntityClass<Track, SpotifyTrack>(TrackTable) {
         override fun Track.update(networkModel: SpotifyTrack) {
+            discNumber = networkModel.discNumber.toUInt()
             durationMs = networkModel.durationMs.toULong() // TODO use ULong in network model?
+            explicit = networkModel.explicit
+            local = networkModel.isLocal
+            playable = networkModel.isPlayable
+            trackNumber = networkModel.trackNumber.toUInt()
+            networkModel.album?.let {
+                album = Album.from(it)
+            }
+
+            if (networkModel is SimplifiedSpotifyTrack) {
+                networkModel.popularity?.let {
+                    popularity = it.toUInt()
+                }
+            }
 
             if (networkModel is FullSpotifyTrack) {
                 fullUpdatedTime = Instant.now()
+                popularity = networkModel.popularity.toUInt()
             }
         }
     }
