6b746283b7b4ecd6317e2385b28530fac2775360
Dominic Zirbel <dominiczirbel@gmail.com>
2023-06-28 22:11:30 -0700

    Avoid recomposition when loading image bitmaps

--- src/main/kotlin/com/dzirbel/kotify/ui/components/LoadedImage.kt
+++ src/main/kotlin/com/dzirbel/kotify/ui/components/LoadedImage.kt
@@ -1,56 +1,66 @@
 package com.dzirbel.kotify.ui.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.Dp
 import com.dzirbel.kotify.cache.SpotifyImageCache
 import com.dzirbel.kotify.ui.theme.Dimens
-import com.dzirbel.kotify.ui.util.callbackAsState
 import com.dzirbel.kotify.ui.util.instrumentation.instrument
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
 /**
- * Wraps [Image] for the given [url] with a loading state, using the [SpotifyImageCache] to fetch the image.
+ * Asynchronously loads an image from [url] via the [SpotifyImageCache] and displays it as a square image with the given
+ * [size] and clipped to [shape].
  */
 @Composable
 fun LoadedImage(
     url: String?,
     modifier: Modifier = Modifier,
     size: Dp = Dimens.contentImage,
-    scope: CoroutineScope = rememberCoroutineScope { Dispatchers.IO },
-    contentDescription: String? = null,
+    shape: Shape = RoundedCornerShape(Dimens.cornerSize),
 ) {
-    val imageState = url?.let {
-        // shortcut the happy path where the image is in memory and doesn't need a recomposition to load it
-        remember(url) {
-            SpotifyImageCache.getInMemory(url)?.let { mutableStateOf(it) }
-        } ?: callbackAsState(key = url) { SpotifyImageCache.get(url = url, scope = scope) }
+    val imageState = remember { mutableStateOf<ImageBitmap?>(null) }
+
+    if (url != null) {
+        val scope = rememberCoroutineScope { Dispatchers.IO }
+        LaunchedEffect(url) {
+            imageState.value = SpotifyImageCache.get(url = url, scope = scope)
+        }
     }
 
-    val imageModifier = modifier
-        .instrument()
-        .size(size)
-        .clip(RoundedCornerShape(Dimens.cornerSize))
-
-    val imageBitmap = imageState?.value
-    if (imageBitmap == null) {
-        Box(imageModifier.background(Color.LightGray))
-    } else {
-        Image(
-            bitmap = imageBitmap,
-            contentDescription = contentDescription,
-            modifier = imageModifier,
-        )
-    }
+    Layout(
+        content = {},
+        modifier = modifier
+            .instrument()
+            .size(size)
+            .clip(shape)
+            .drawWithCache {
+                val painter = imageState.value?.let { BitmapPainter(it) }
+                onDrawWithContent {
+                    val contentSize = this.size
+                    if (painter == null) {
+                        drawRect(color = Color.LightGray, topLeft = Offset.Zero, size = contentSize)
+                    } else {
+                        with(painter) { draw(contentSize) }
+                    }
+                }
+            },
+        measurePolicy = { _, constraints ->
+            layout(constraints.minWidth, constraints.minHeight) {}
+        },
+    )
 }
