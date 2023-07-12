4b7b577c4120f1834e2b0c99bc54ef8cfe286209
Dominic Zirbel <dominiczirbel@gmail.com>
2023-07-11 23:19:20 -0700

    Log application startup time

--- application/src/main/kotlin/com/dzirbel/kotify/Main.kt
+++ application/src/main/kotlin/com/dzirbel/kotify/Main.kt
@@ -16,49 +16,78 @@ import com.dzirbel.kotify.ui.KeyboardShortcuts
 import com.dzirbel.kotify.ui.Root
 import com.dzirbel.kotify.ui.SpotifyImageCache
 import okhttp3.OkHttpClient
+import kotlin.time.Duration
+import kotlin.time.measureTime
 
 fun main(args: Array<String>) {
-    // TODO log startup time
+    val initTimes = mutableListOf<Pair<String, Duration>>()
 
-    Application.setup(
-        cachePath = args.getOrNull(0),
-        settingsPath = args.getOrNull(1),
+    fun measureInitTime(name: String, block: () -> Unit) {
+        initTimes.add(name to measureTime(block))
+    }
+
+    val totalDuration = measureTime {
+        measureInitTime("file system") {
+            Application.setup(
+                cachePath = args.getOrNull(0),
+                settingsPath = args.getOrNull(1),
+            )
+        }
+
+        measureInitTime("settings") {
+            Settings.ensureLoaded()
+        }
+
+        measureInitTime("database") {
+            KotifyDatabase.init(
+                dbFile = Application.cacheDir.resolve("cache.db"),
+                sqlLogger = Logger.Database,
+                onConnect = {
+                    UserRepository.currentUserId.loadToCache()
+                    com.dzirbel.kotify.repository2.user.UserRepository.onConnectToDatabase()
+                },
+            )
+
+            KotifyDatabase.addTransactionListener(Logger.Database)
+        }
+
+        measureInitTime("network") {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(Logger.Network::intercept)
+                .addInterceptor(DelayInterceptor)
+                .build()
+
+            Spotify.configuration = Spotify.Configuration(
+                okHttpClient = okHttpClient,
+                oauthOkHttpClient = okHttpClient,
+            )
+        }
+
+        measureInitTime("player init") {
+            // load initial player state
+            PlayerRepository.refreshPlayback()
+            PlayerRepository.refreshTrack()
+            PlayerRepository.refreshDevices()
+        }
+
+        measureInitTime("access token") {
+            AccessToken.Cache.cacheFile = Application.cacheDir.resolve("access_token.json")
+            AccessToken.Cache.requireRefreshable() // clear non-refreshable tokens from tests
+        }
+
+        measureInitTime("image cache") {
+            SpotifyImageCache.init(imagesDir = Application.cacheDir.resolve("images"))
+        }
+
+        measureInitTime("icon cache") {
+            IconCache.loadBlocking = false
+        }
+    }
+
+    Logger.Events.info(
+        title = "Application initialized in $totalDuration",
+        content = initTimes.joinToString(separator = "\n") { (name, duration) -> "$name : $duration" },
     )
-
-    Settings.ensureLoaded()
-
-    KotifyDatabase.init(
-        dbFile = Application.cacheDir.resolve("cache.db"),
-        sqlLogger = Logger.Database,
-        onConnect = {
-            UserRepository.currentUserId.loadToCache()
-            com.dzirbel.kotify.repository2.user.UserRepository.onConnectToDatabase()
-        },
-    )
-
-    KotifyDatabase.addTransactionListener(Logger.Database)
-
-    val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(Logger.Network::intercept)
-        .addInterceptor(DelayInterceptor)
-        .build()
-
-    Spotify.configuration = Spotify.Configuration(
-        okHttpClient = okHttpClient,
-        oauthOkHttpClient = okHttpClient,
-    )
-
-    // load initial player state
-    PlayerRepository.refreshPlayback()
-    PlayerRepository.refreshTrack()
-    PlayerRepository.refreshDevices()
-
-    AccessToken.Cache.cacheFile = Application.cacheDir.resolve("access_token.json")
-    AccessToken.Cache.requireRefreshable() // clear non-refreshable tokens from tests
-
-    SpotifyImageCache.init(imagesDir = Application.cacheDir.resolve("images"))
-
-    IconCache.loadBlocking = false
 
     application {
         Window(
