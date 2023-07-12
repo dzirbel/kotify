49d512be5d86a303bafc02009e551108b7104293
Dominic Zirbel <dominiczirbel@gmail.com>
2023-07-11 23:07:37 -0700

    Document and unit test BackoffStrategy

--- repository2/src/main/kotlin/com/dzirbel/kotify/repository2/util/BackoffStrategy.kt
+++ repository2/src/main/kotlin/com/dzirbel/kotify/repository2/util/BackoffStrategy.kt
@@ -3,18 +3,29 @@ package com.dzirbel.kotify.repository2.util
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
-// TODO document
-// TODO test
+/**
+ * Provides a strategy for retrying failed calls.
+ */
 internal interface BackoffStrategy {
+    /**
+     * Returns the [Duration] to delay after a failure for the given [attempt], starting with 0 for a failure on the
+     * first attempt. Returns null if the operation should be aborted and no retry made.
+     */
     fun delayFor(attempt: Int): Duration?
 
-    class OfDelays(private vararg val delays: Long) : BackoffStrategy {
+    /**
+     * A simple [BackoffStrategy] determined by [delaysMs].
+     */
+    class OfDelays(private vararg val delaysMs: Long) : BackoffStrategy {
         override fun delayFor(attempt: Int): Duration? {
-            return delays.getOrNull(attempt)?.milliseconds
+            return delaysMs.getOrNull(attempt)?.milliseconds
         }
     }
 
     companion object {
+        /**
+         * The default [BackoffStrategy] with a fixed number of delays of increasing duration.
+         */
         val default = OfDelays(250, 500, 500, 2000, 5000)
     }
 }
