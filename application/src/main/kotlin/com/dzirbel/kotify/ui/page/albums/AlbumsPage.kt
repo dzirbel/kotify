0c88aa0ab2115d80f4a20cfd5c3da8db00a22721
Dominic Zirbel <dominiczirbel@gmail.com>
2023-07-24 14:52:47 -0700

    Convert albums page to new repositories

--- application/src/main/kotlin/com/dzirbel/kotify/ui/page/albums/AlbumsPage.kt
+++ application/src/main/kotlin/com/dzirbel/kotify/ui/page/albums/AlbumsPage.kt
@@ -1,21 +1,76 @@
 package com.dzirbel.kotify.ui.page.albums
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import com.dzirbel.kotify.ui.framework.PresenterPage
-import kotlinx.coroutines.CoroutineScope
+import androidx.compose.ui.Modifier
+import com.dzirbel.kotify.repository2.album.AlbumRepository
+import com.dzirbel.kotify.repository2.album.SavedAlbumRepository
+import com.dzirbel.kotify.ui.components.AlbumCell
+import com.dzirbel.kotify.ui.components.LibraryInvalidateButton
+import com.dzirbel.kotify.ui.components.PageLoadingSpinner
+import com.dzirbel.kotify.ui.components.adapter.rememberListAdapterState
+import com.dzirbel.kotify.ui.components.grid.Grid
+import com.dzirbel.kotify.ui.framework.Page
+import com.dzirbel.kotify.ui.framework.VerticalScrollPage
+import com.dzirbel.kotify.ui.page.album.AlbumPage
+import com.dzirbel.kotify.ui.pageStack
+import com.dzirbel.kotify.ui.properties.AlbumNameProperty
+import com.dzirbel.kotify.ui.theme.Dimens
+import com.dzirbel.kotify.ui.util.derived
+import com.dzirbel.kotify.ui.util.mutate
+import com.dzirbel.kotify.util.combinedStateWhenAllNotNull
+import com.dzirbel.kotify.util.flatMapLatestIn
+import kotlinx.coroutines.flow.MutableStateFlow
 
-object AlbumsPage : PresenterPage<AlbumsPresenter.ViewModel, AlbumsPresenter>() {
-    override fun createPresenter(scope: CoroutineScope) = AlbumsPresenter(scope)
-
+object AlbumsPage : Page<Unit>() {
     @Composable
-    override fun Header(presenter: AlbumsPresenter, state: AlbumsPresenter.ViewModel) {
-        AlbumsPageHeader(presenter, state)
+    override fun BoxScope.bind(visible: Boolean) {
+        val albumsAdapter = rememberListAdapterState(defaultSort = AlbumNameProperty) { scope ->
+            SavedAlbumRepository.library.flatMapLatestIn(scope) { library ->
+                library?.ids
+                    ?.let { albumIds ->
+                        AlbumRepository.statesOf(albumIds).combinedStateWhenAllNotNull { it?.cachedValue }
+                    }
+                    ?: MutableStateFlow(null)
+            }
+        }
+
+        VerticalScrollPage(
+            visible = visible,
+            onHeaderVisibilityChanged = { headerVisible -> navigationTitleState.targetState = !headerVisible },
+            header = {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(Dimens.space4),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text("Albums", style = MaterialTheme.typography.h5)
+
+                    Column {
+                        LibraryInvalidateButton(savedRepository = SavedAlbumRepository)
+                    }
+                }
+            },
+            content = {
+                if (albumsAdapter.derived { it.hasElements }.value) {
+                    Grid(elements = albumsAdapter.value) { _, album ->
+                        AlbumCell(
+                            album = album,
+                            onClick = { pageStack.mutate { to(AlbumPage(albumId = album.id.value)) } },
+                        )
+                    }
+                } else {
+                    PageLoadingSpinner()
+                }
+            },
+        )
     }
 
-    @Composable
-    override fun Content(presenter: AlbumsPresenter, state: AlbumsPresenter.ViewModel) {
-        AlbumsPageContent(presenter, state)
-    }
-
-    override fun titleFor(data: AlbumsPresenter.ViewModel) = "Saved Albums"
+    override fun titleFor(data: Unit) = "Saved Albums"
 }
