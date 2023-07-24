a8b979db4fdbd627311813cd7d8911830d561df6
Dominic Zirbel <dominiczirbel@gmail.com>
2023-07-24 14:55:43 -0700

    Delete debug library pages

--- application/src/main/kotlin/com/dzirbel/kotify/ui/panel/library/LibraryPanel.kt
+++ application/src/main/kotlin/com/dzirbel/kotify/ui/panel/library/LibraryPanel.kt
@@ -1,22 +1,16 @@
 package com.dzirbel.kotify.ui.panel.library
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,7 +27,6 @@ import com.dzirbel.kotify.ui.components.VerticalScroll
 import com.dzirbel.kotify.ui.components.VerticalSpacer
 import com.dzirbel.kotify.ui.page.albums.AlbumsPage
 import com.dzirbel.kotify.ui.page.artists.ArtistsPage
-import com.dzirbel.kotify.ui.page.library.LibraryStatePage
 import com.dzirbel.kotify.ui.page.playlist.PlaylistPage
 import com.dzirbel.kotify.ui.pageStack
 import com.dzirbel.kotify.ui.theme.Dimens
@@ -46,39 +39,11 @@ import com.dzirbel.kotify.util.zipEach
 fun LibraryPanel() {
     LocalColors.current.WithSurface {
         VerticalScroll(Modifier.surfaceBackground()) {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(Dimens.space3),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "Library",
-                    style = MaterialTheme.typography.h5,
-                    modifier = Modifier.padding(start = Dimens.space3, end = Dimens.space3, top = Dimens.space3),
-                )
-
-                val moreExpanded = remember { mutableStateOf(false) }
-                IconButton(
-                    modifier = Modifier.size(Dimens.iconSmall),
-                    onClick = { moreExpanded.value = true },
-                ) {
-                    CachedIcon(name = "more-vert", contentDescription = "More", size = Dimens.iconSmall)
-
-                    DropdownMenu(
-                        expanded = moreExpanded.value,
-                        onDismissRequest = { moreExpanded.value = false },
-                    ) {
-                        DropdownMenuItem(
-                            onClick = {
-                                moreExpanded.value = false
-                                pageStack.mutate { to(LibraryStatePage) }
-                            },
-                        ) {
-                            Text("Details")
-                        }
-                    }
-                }
-            }
+            Text(
+                text = "Library",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(start = Dimens.space3, end = Dimens.space3, top = Dimens.space3),
+            )
 
             HorizontalDivider(Modifier.padding(bottom = Dimens.space3))
 
