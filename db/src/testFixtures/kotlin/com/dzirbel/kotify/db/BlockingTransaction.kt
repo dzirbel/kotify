6e5034345719705c5fa7dbf9b36918609bcfa4ec
Dominic Zirbel <dominiczirbel@gmail.com>
2024-04-14 00:02:18 -0700

    Upgrade Kotlin 1.9.23 -> 2.0.0-RC1

--- db/src/testFixtures/kotlin/com/dzirbel/kotify/db/BlockingTransaction.kt
+++ db/src/testFixtures/kotlin/com/dzirbel/kotify/db/BlockingTransaction.kt
@@ -8,6 +8,7 @@ import kotlin.contracts.contract
 /**
  * Blocking wrapper around [KotifyDatabase.DatabaseContext.transaction] for use in tests.
  */
+@Suppress("LEAKED_IN_PLACE_LAMBDA", "WRONG_INVOCATION_KIND") // false positives (probably) in Kotlin 2.0.0-RC1
 fun <T> KotifyDatabase.blockingTransaction(db: DB = DB.CACHE, statement: Transaction.() -> T): T {
     contract {
         callsInPlace(statement, InvocationKind.EXACTLY_ONCE)
